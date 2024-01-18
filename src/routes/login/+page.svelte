36c92e70a1aea44da4e78ebf99bef8c20087506f
Alexander Du <du.alexander123@gmail.com>
2024-01-17 19:16:31 -0800

    basic register/login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,13 +2,30 @@
 	let email = '';
 	let password = '';
 
-	function handleLogin() {
+	const handleLogin = async () => {
+		const res = await fetch('/api/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ email, password })
+		});
+
+		const data = await res.json();
+
+		if (res.ok) {
+			console.log('success');
+			console.log(data);
+		} else {
+			console.log('error');
+			console.error(data);
+		}
 		// Handle login logic here
 		console.log(`Email: ${email}, Password: ${password}`);
-	}
+	};
 </script>
 
-<form on:submit|preventDefault={handleLogin}>
+<form method="POST" on:submit|preventDefault={handleLogin}>
 	<label for="email">Email:</label>
 	<input id="email" type="email" bind:value={email} required />
 
