c3b42716b0df51f2bf019ab9ace6da0ed9f8418f
Alexander Du <du.alexander123@gmail.com>
2024-06-03 00:29:23 -0700

    user stats

--- src/routes/api/updateStats/+server.js
+++ src/routes/api/updateStats/+server.js
@@ -13,36 +13,40 @@ const math = questions.collection("math");
 export const POST = async ({request}) => {
     const res = await request.json();
     let { question, user, selectedAnswer } = res;
+    let isCorrect = selectedAnswer == question.question.correctAnswer || question.question.correctAnswer.includes(selectedAnswer);
 
-    let correctAnswer = selectedAnswer == question.question.correctAnswer || question.question.correctAnswer.includes(selectedAnswer);
-    
-    if(question.section === "Reading") {
-        let newRating = updateRating(user.stats.rating, question.difficulty, correctAnswer);
+    let collection = question.section === "Reading" ? rw : math;
+    let userCollection = question.section === "Reading" ? "reading" : "math";
 
-        await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
-            "stats.rating": newRating,
+    let newRating = updateRating(user.stats.rating, question.difficulty, isCorrect);
+
+    await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
+        "stats.rating": newRating,
+    }});
+
+    let newSectionRating = updateRating(user[userCollection].rating, question.difficulty, isCorrect);
+    await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
+        [userCollection + ".rating"]: newSectionRating,
+    }});
+    if(isCorrect) {
+        await collection.updateOne({ _id: new ObjectId(question._id) }, { $set: {
+            "stats.correct": question.stats.correct + 1
         }});
-        if(correctAnswer) {
-            await rw.updateOne({ _id: new ObjectId(question._id) }, { $set: {
-                "stats.correct": question.stats.correct + 1
-            }});
-            await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
-                "stats.correct": user.stats.correct + 1,
-            }});
-        } else {
-            await rw.updateOne({ _id: new ObjectId(question._id) }, { $set: {
-                "stats.incorrect": question.stats.incorrect + 1
-            }});
-            await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
-                "stats.incorrect": user.stats.incorrect + 1,
-            }});
-        }
-
-
+        await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
+            "stats.correct": user.stats.correct + 1,
+            [userCollection + ".correct"]: user[userCollection].correct + 1
+        }});
+    } else {
+        await collection.updateOne({ _id: new ObjectId(question._id) }, { $set: {
+            "stats.incorrect": question.stats.incorrect + 1
+        }});
+        await users.updateOne({ _id: new ObjectId(user._id) }, { $set: {
+            "stats.incorrect": user.stats.incorrect + 1,
+            [userCollection + ".incorrect"]: user[userCollection].incorrect + 1
+        }});
     }
 
 
-    // console.log("THINGY", res);
-
+    
     return new Response(JSON.stringify(res), { status: 201 });
 }
