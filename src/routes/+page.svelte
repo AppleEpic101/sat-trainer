dcc90f2eca636d7c2d4427e56f224d0ae16e9ec6
Alexander Du <du.alexander123@gmail.com>
2024-06-14 02:10:41 -0700

    skeleton ui config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,29 @@
-<div class="bg-black h-screen flex justify-center items-center">
-	<div class="font-bold text-4xl text-white">Sigma SAT</div>
+<div class="bg-sky-950 h-screen flex flex-col gap-4 justify-center items-center">
+	<img src="/logo.png" class="w-28 h-28" />
+	<div class="font-bold text-5xl text-white">Sigma SAT</div>
+	<div class="text-3xl text-white">Free, adaptive trainer for the Digital SAT</div>
 </div>
-<div>
-	For 98 years, College Board's SAT was a paper-based standardized test. In January 2022, the
+<div class="bg-cyan-800 flex flex-col gap-4 items-center py-12">
+	<div class="font-bold text-5xl text-white">Questionbank</div>
+	<div class="text-3xl text-white">
+		Thousands of questions, organized by topic, sourced from CollegeBoard and volunteers
+	</div>
+	<div class="flex flex-row gap-28 text-white mt-8">
+		<div class="flex flex-col gap-1 text-white items-center">
+			<div class="font-bold text-3xl text-white">Questions</div>
+			<div class="text-2xl text-white">2017</div>
+		</div>
+		<div class="flex flex-col gap-1 text-white items-center">
+			<div class="font-bold text-3xl text-white">Users</div>
+			<div class="text-2xl text-white">200</div>
+		</div>
+		<div class="flex flex-col gap-1 text-white items-center">
+			<div class="font-bold text-3xl text-white">Voluneers</div>
+			<div class="text-2xl text-white">20</div>
+		</div>
+	</div>
+</div>
+
+<!-- For 98 years, College Board's SAT was a paper-based standardized test. In January 2022, the
 	College Board announced a switch to a digital version. Sigma SAT is a tool for pre-college
-	students
-</div>
+	students -->
