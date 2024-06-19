efce7c44c5da77d7d2bb6a80489102cfa36519e1
Alexander Du <du.alexander123@gmail.com>
2024-06-18 20:47:12 -0700

    leaderboard css

--- src/lib/components/Leaderboard.svelte
+++ src/lib/components/Leaderboard.svelte
@@ -5,9 +5,25 @@
 	export let section;
 </script>
 
-<div>{skill}</div>
-{#each data as user, i}
-	{i + 1})
-	{user.username}
-	{user[section][domain][skill].experience}
-{/each}
+<div class="text-lg">{skill}</div>
+
+<div class="overflow-x-auto relative">
+	<table class="w-96 text-sm text-left">
+		<thead class="text-xs text-gray-700 uppercase bg-emerald-400">
+			<tr>
+				<th scope="col" class="py-2 px-6">Rank</th>
+				<th scope="col" class="py-2 px-6">Username</th>
+				<th scope="col" class="py-2 px-6">Level</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data as user, i}
+				<tr class="bg-white border">
+					<td class="py-1 px-6">{i + 1}</td>
+					<td class="py-1 px-6">{user.username}</td>
+					<td class="py-1 px-6">{user[section][domain][skill].experience}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -6,20 +6,26 @@
 	let { readingLeaderboard } = data;
 </script>
 
-{#each data.overall as user}
-	<div>
-		{user.username}
-		{user.stats.rating}
-	</div>
-{/each}
-
-<div>Reading</div>
-
-{#each Object.keys(READING_SKILLS) as domain}
-	<div>{domain}</div>
-	{#each READING_SKILLS[domain] as skill}
+<div class="mx-8 my-4">
+	{#each data.overall as user}
 		<div>
-			<Leaderboard data={readingLeaderboard[skill]} {domain} {skill} section={'reading'} />
+			{user.username}
+			{user.stats.rating}
 		</div>
 	{/each}
-{/each}
+
+	<div>Reading</div>
+
+	{#each Object.keys(READING_SKILLS) as domain}
+		<div class="text-xl text-center">{domain}</div>
+		<div class="w-full border border-black my-4 p-4">
+			<div class="flex flex-row justify-around gap-4">
+				{#each READING_SKILLS[domain] as skill}
+					<div>
+						<Leaderboard data={readingLeaderboard[skill]} {domain} {skill} section={'reading'} />
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
