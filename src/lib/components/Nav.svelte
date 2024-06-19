84f45493f457f4cec2209f8a3eb00a3bdf97cd64
Alexander Du <du.alexander123@gmail.com>
2024-06-19 01:12:01 -0700

    fix ui

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -17,7 +17,51 @@
 	$: loggedIn = !cookies.error;
 </script>
 
-<Navbar class="bg-cyan-500">
+<nav class="bg-cyan-500 flex flex-row justify-between items-center h-20">
+	<a class="flex flex-row ml-24" href="/">
+		<img src="/logo.png" class="me-3 h-12 sm:h-10" alt="Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold">Sigma SAT</span>
+	</a>
+	<div class="flex flex-row gap-8">
+		<a href="/" class="text-lg font-semibold">Home</a>
+		<a href="/about" class="text-lg font-semibold">About</a>
+		{#if loggedIn}
+			<a href="/problems" class="text-lg font-semibold">Problems</a>
+			<a href="/train" class="text-lg font-semibold">Train</a>
+			<a href="/stats" class="text-lg font-semibold">Stats</a>
+		{/if}
+		<a href="/contact" class="text-lg font-semibold">Contact</a>
+	</div>
+
+	{#if loggedIn}
+		<div class="flex items-center mr-24 md:order-2 hover:cursor-pointer">
+			<Avatar id="avatar-menu" src="/pfp.jpg" />
+			<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+		</div>
+		<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+			<DropdownHeader>
+				<span class="block text-sm">{cookies.username}</span>
+				<span class="block truncate text-sm font-medium">{cookies.email}</span>
+			</DropdownHeader>
+			<DropdownItem href="/dashboard">Dashboard</DropdownItem>
+			<DropdownItem href="/settings">Settings</DropdownItem>
+			<DropdownDivider />
+
+			<form method="post" action="/logout">
+				<button type="submit" class="w-full">
+					<DropdownItem>Log out</DropdownItem>
+				</button>
+			</form>
+		</Dropdown>
+	{:else}
+		<div class="flex flex-row gap-8">
+			<div class="text-lg font-semibold">Login</div>
+			<div class="text-lg font-semibold">Register</div>
+		</div>
+	{/if}
+</nav>
+
+<!-- <Navbar class="bg-cyan-500">
 	<NavBrand href="/">
 		<img src="/logo.png" class="me-3 h-12 sm:h-10" alt="Logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
@@ -62,4 +106,4 @@
 			<NavLi class="text-lg" href="/register">Register</NavLi>
 		</NavUl>
 	{/if}
-</Navbar>
+</Navbar> -->
--- src/routes/train/+page.svelte
+++ src/routes/train/+page.svelte
@@ -1,14 +1,22 @@
-<div class="flex flex-row justify-evenly align my-8">
-	<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/math">Math</a
-	>
-	<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/rw"
-		>Reading/Writing</a
-	>
+<div class="mx-8 my-4">
+	<div class="text-2xl">Classic Mode</div>
+	<div class="flex flex-row justify-evenly align my-8">
+		<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/classic-rw"
+			>Classic Reading</a
+		>
+		<a
+			class="font-bold text-2xl border-black bg-slate-600 text-white p-10"
+			href="/train/classic-math">Classic Math</a
+		>
+	</div>
+	<div class="text-2xl">Unrated Practice Mode</div>
 
-	<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/classic-rw"
-		>Classic Reading</a
-	>
-	<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/classic-math"
-		>Classic Math</a
-	>
+	<div class="flex flex-row justify-evenly align my-8">
+		<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/rw"
+			>Unrated Reading</a
+		>
+		<a class="font-bold text-2xl border-black bg-slate-600 text-white p-10" href="/train/math"
+			>Unrated Math</a
+		>
+	</div>
 </div>
