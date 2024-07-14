2bb8685a66bff20dac197bdfa16f13593e9619e1
Alexander Du <du.alexander123@gmail.com>
2024-07-14 01:27:04 -0400

    sigma

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,14 +1,16 @@
 <script>
 	export let data;
 
-	const { stats, reading, math, username } = data.user;
+	const { stats, log, username } = data.user;
+
+	const reading = log['All Reading Topics'].stats;
+	const math = log['All Math Topics'].stats;
 </script>
 
 <div class="bg-slate-300 border-black m-8 p-4">
 	<div class="text-4xl font-bold">Welcome {username}!</div>
 	<div class="text-2xl mt-4">General Stats</div>
 	<div class="flex flex-col ml-4">
-		<div>Rating: {stats.rating}</div>
 		<div>Attempted Questions: {stats.correct + stats.incorrect + stats.skipped}</div>
 		<div>Correct: {stats.correct}</div>
 		<div>Incorrect: {stats.incorrect}</div>
@@ -17,8 +19,9 @@
 
 	<div class="text-2xl mt-4">Reading Stats</div>
 	<div class="flex flex-col ml-4">
-		<div>Rating: {reading.rating}</div>
-		<div>Attempted Questions: {reading.correct + reading.incorrect + reading.skipped}</div>
+		<div>
+			Attempted Questions: {reading.correct + reading.incorrect + reading.skipped}
+		</div>
 		<div>Correct: {reading.correct}</div>
 		<div>Incorrect: {reading.incorrect}</div>
 		<div>Skipped: {reading.skipped}</div>
@@ -26,8 +29,9 @@
 
 	<div class="text-2xl mt-4">Math Stats</div>
 	<div class="flex flex-col ml-4">
-		<div>Rating: {math.rating}</div>
-		<div>Attempted Questions: {math.correct + math.incorrect + math.skipped}</div>
+		<div>
+			Attempted Questions: {math.correct + math.incorrect + math.skipped}
+		</div>
 		<div>Correct: {math.correct}</div>
 		<div>Incorrect: {math.incorrect}</div>
 		<div>Skipped: {math.skipped}</div>
