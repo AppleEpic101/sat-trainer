e51c9111861d8076d77ea871ecfcde0575f0257f
Alexander Du <du.alexander123@gmail.com>
2024-07-26 01:18:10 -0400

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<div class="flex flex-col mx-4 my-8">
+	<h1 class="text-3xl font-bold text-center text-cyan-600">About Us</h1>
+	<p class="text-xl text-center my-8">
+		Sigma SAT is a nonprofit organization that is dedicated to providing a free, online, adaptive
+		training environment for College Board's Digital SAT. Deviating from their 98 years of tradition
+		of paper-based examinations, College Board implemented sweeping changes to the SAT by shortening
+		its length and transitioning to a fully digital format. As of July 2024, resources for the
+		Digital SAT are far and few in between. We aim to bridge this gap by providing high-quality,
+		accessible resources to equip students for test day.
+	</p>
+	<p class="text-xl text-center my-4">
+		Sigma SAT is a community-driven initiative. Whether you are currently studying for the SAT or
+		you have already achieved your dream score, there is a place for you at Sigma SAT. Anybody may
+		contribute to our vast question database comprising of over 1500+ questions, and in return earn
+		volunteer hours. Some questions are sourced from official College Board via their educator's
+		questionbank. Others are either original questions by volunteers or inspired from previous
+		Practice Exams and QAS from 2016-2023.
+	</p>
+</div>
