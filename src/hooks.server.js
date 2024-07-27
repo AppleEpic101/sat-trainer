31ecbc50875a96bd3bb9378d3c356353eedd852e
Alexander Du <du.alexander123@gmail.com>
2024-07-27 18:06:30 -0400

    protecting my pages

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,39 @@
 import { authenticate } from '$lib/server/authenticate';
 import { redirect } from '@sveltejs/kit';
 
+
+import { MongoClient, ObjectId } from "mongodb";
+import { MONGO_STRING } from "$env/static/private";
+
+const client = await MongoClient.connect(MONGO_STRING);
+const db = client.db("accounts");
+const collection = db.collection("users");
+
+const loginProtected = ['/train', '/dashboard', '/settings', '/review', '/create', '/question']
+const reviewerProtected = ['/review', '/create', '/question']
+const adminProtected = ['/review', '/create', '/question']
+
 export const handle = async ({ event, resolve }) => {
-    const isProtected = event.url.pathname.startsWith('/train');
+    const pathname = event.url.pathname;
     const auth = authenticate(event.cookies);
 
-    if(isProtected && !auth) {
-        throw redirect(307, "/login");
-	}
+    if (!auth) {
+        if (loginProtected.some(route => pathname.startsWith(route))) {
+            throw redirect(307, "/login");
+        }
+    } else {
+        const user = await collection.findOne({ _id: new ObjectId(auth.id) });
+        const isAdmin = user.meta.isAdmin;
+        const isReviewer = user.meta.isReviewer;
+
+        if (adminProtected.some(route => pathname.startsWith(route)) && !isAdmin) {
+            throw redirect(307, "/");
+        }
+
+        if (reviewerProtected.some(route => pathname.startsWith(route)) && !isReviewer) {
+            throw redirect(307, "/");
+        }
+    }
 
 	const response = await resolve(event);
 	return response;
